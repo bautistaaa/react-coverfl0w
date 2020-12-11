f55f031646c433f3f81c9ebd448bcb0b1222dc26
chris-bautista <chris.bautista@hulu.com>
2020-12-11 14:10:55 -0800

    chore: chanes exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export { default as Coverflow } from './Coverflow';
+'use strict';
 export { CoverflowProps, Images } from './types';
+
+const Coverflow = require('./Coverflow');
+
+module.exports = Coverflow.default || Coverflow;
